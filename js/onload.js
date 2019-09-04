79263fb1001a72602756e7418d4e25c44884b037
jakubwirfel <jakubwirfel@gmail.com>
2019-09-04 10:41:23 +0200

    Portfolio pages finished

--- js/onload.js
+++ js/onload.js
@@ -1,15 +1,10 @@
 var particles = document.getElementById('particles-js');
-var image = document.getElementById('image');
-var width = window.innerWidth;
 
 aboutusload = function () {
     particles.style.backgroundColor = ('#00A4CCFF');
  }
 workload = function () {
     particles.style.backgroundColor = ('#161B21');
-    if (width >= 560) {
-        image.src = 'assets/Ogrodnictwo2.png';
-    }
  }
 contactload = function () {
     particles.style.backgroundColor = ('#EC8B5E');
