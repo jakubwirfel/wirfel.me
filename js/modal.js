d78b8ed482a63e9f5b90bae3355bf60c2751df55
jakubwirfel <jakubwirfel@gmail.com>
2019-09-23 16:33:42 +0200

    Form contact responsive and finished

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll(`button[data-modal-trigger]`);
+const html = document.querySelector('html');
 
 for(let button of buttons) {
 	modalEvent(button);
@@ -7,11 +8,13 @@ for(let button of buttons) {
 function modalEvent(button) {
 	button.addEventListener('click', () => {
 		const trigger = button.getAttribute('data-modal-trigger');
+		html.style.overflow = ('hidden');
 		const modal = document.querySelector(`[data-modal=${trigger}]`);
 		const contentWrapper = modal.querySelector('.content-wrapper');
 		const close = modal.querySelector('.close');
 
 		close.addEventListener('click', () => modal.classList.remove('open'));
+		close.addEventListener('click', () =>html.style.overflow = ('inherit'));
 		modal.addEventListener('click', () => modal.classList.remove('open'));
 		contentWrapper.addEventListener('click', (e) => e.stopPropagation());
 
