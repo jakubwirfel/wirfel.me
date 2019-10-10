3c4a0e712dcba6a2d8569ea76e9e01fe946ac4e3
jakubwirfel <jakubwirfel@gmail.com>
2019-10-10 16:23:26 +0200

    PHP fixed and add

--- php/wycena.php
+++ php/wycena.php
@@ -8,8 +8,9 @@
     $url_liked1 = $_REQUEST["url_liked1"];
     $url_liked2 = $_REQUEST["url_liked2"];
     $toggle2 = $_REQUEST["toggle2"];
+    $toggle3 = $_REQUEST["toggle3"];
    
-    $zawartosc_email = "<b>Bezpłatna wycena</b><br/><br/>Adres email nadawcy: <b>".$email."</b><br/><br/>System CMS: <b>".$toggle."</b><br/><br/>Orientacyjna liczba podstron: <b>".$range_www."</b><br/><br/>Dodatkowe wersje językowe: <b>".$lang."</b> Ilość:  <b>".$range_lang."</b><br/><br/>Adres aktualnej strony internetowej: <b>".$url_website."</b><br/><br/>Linki do stron, które Ci się podobają: <b><br/>".$url_liked1."</b><br/><b>".$url_liked1."</b><br/><br/>Czy mam zredagować treści na twoją stronę www?: <b>".$toggle2."</b>";                                   
+    $zawartosc_email = "<b>Bezpłatna wycena</b><br/><br/>Adres email nadawcy: <b>".$email."</b><br/><br/>System CMS: <b>".$toggle."</b><br/><br/>Orientacyjna liczba podstron: <b>".$range_www."</b><br/><br/>Dodatkowe wersje językowe: <b>".$lang."</b> Ilość:  <b>".$range_lang."</b><br/><br/>Adres aktualnej strony internetowej: <b>".$url_website."</b><br/><br/>Linki do stron, które Ci się podobają: <b><br/>".$url_liked1."</b><br/><b>".$url_liked1."</b><br/><br/>Czy mam zredagować treści na twoją stronę www?: <b>".$toggle2."</b><br/><br/>Czy mam wykonać grafiki na twoją stronę internetową?: <b>".$toggle3."</b>";                                   
 
     $adres_do = "jakubwirfel@gmail.com";
     $adres_od = $email;
